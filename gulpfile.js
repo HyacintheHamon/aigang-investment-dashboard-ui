84b0f2675dfdaf25581cdd0fe0fb89ec63b801d7
Darius Devenas <d.devenas@adform.com>
2017-07-11 22:54:55 +0300

    add build site with js versions

--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,17 @@
 var gulp = require('gulp')
 	, gutil = require('gulp-util')
 	, watch = require('gulp-watch')
-	, ftp = require('vinyl-ftp')
 	//, sass = require('gulp-sass')
 	, uglify = require('gulp-uglify')
 	, concat = require('gulp-concat')
 	, rename = require('gulp-rename')
+	, clean = require('gulp-clean')
 	, clean_css = require('gulp-clean-css')
 	, include = require('gulp-include')
-	, autoprefixer = require('gulp-autoprefixer');
+	, cleanDest = require('gulp-clean-dest')
+	, htmlreplace = require('gulp-html-replace')
+	, autoprefixer = require('gulp-autoprefixer')
+	, htmlmin = require('gulp-htmlmin');
 
 
 var asset_folder_css = 'assets/sass';
@@ -18,9 +21,10 @@ var asset_folder_js = 'assets/js';
 var dest_folder_css = 'css';
 var dest_folder_js = 'js';
 var dest_folder_build = 'build';
-var dest_folder_build_js = 'build/js';
 
-var version = '20170711'
+
+
+var version = '201707112128'
 
 
 
@@ -37,32 +41,31 @@ gulp.task('compile_sass', function () {
 });
 
 
-gulp.task('process_css', ['compile_sass'], function () {
+// gulp.task('process_css', ['compile_sass'], function () {
 
-	gulp.src([dest_folder_css + '/*.css', '!' + dest_folder_css + '/*.min.css'])
-		.pipe(clean_css({ compatibility: 'ie8' }))
-		.pipe(rename({ suffix: ".min" }))
-		.pipe(gulp.dest(dest_folder_css));
+// 	gulp.src([dest_folder_css + '/*.css', '!' + dest_folder_css + '/*.min.css'])
+// 		.pipe(clean_css({ compatibility: 'ie8' }))
+// 		.pipe(rename({ suffix: ".min" }))
+// 		.pipe(gulp.dest(dest_folder_css));
 
-});
+// });
 
 
-gulp.task('minimize_js', function () {
+// gulp.task('minimize_js', function () {
 
-	gulp.src([asset_folder_js + '/*.js', '!' + asset_folder_js + '/*.min.js'])
-		.pipe(include({ extensions: "js" }))
-		.pipe(concat('site-scripts.js'))
-		.pipe(gulp.dest(dest_folder_js))
+// 	gulp.src([asset_folder_js + '/*.js', '!' + asset_folder_js + '/*.min.js'])
+// 		.pipe(include({ extensions: "js" }))
+// 		.pipe(concat('site-scripts.js'))
+// 		.pipe(gulp.dest(dest_folder_js))
 
-		.pipe(rename('site-scripts.min.js'))
-		.pipe(uglify())
-		.pipe(gulp.dest(dest_folder_js))
-});
+// 		.pipe(rename('site-scripts.min.js'))
+// 		.pipe(uglify())
+// 		.pipe(gulp.dest(dest_folder_js))
+// });
 
 gulp.task('minimize_js2', function () {
 	gulp.src(['site-scripts.js'])
 		.pipe(include({ extensions: "js" }))
-		.pipe(concat('site-scripts.js'))
 		.pipe(gulp.dest(dest_folder_build_js))
 
 		.pipe(rename('site-scripts.min.js'))
@@ -79,11 +82,37 @@ gulp.task('minimize_js2', function () {
 		.pipe(gulp.dest(dest_folder_build_js))
 });
 
-gulp.task('buildsite', function () {
+gulp.task('clean-build', function () {
+	return gulp.src(dest_folder_build, { read: false })
+		.pipe(clean());
+});
+
+gulp.task('buildsite', ['clean-build'], function () {
+
+	gulp.src(['fonts/**/*.*', 'images/**/*.*'], { base: "." })
+		.pipe(gulp.dest(dest_folder_build));
+
+	gulp.src([dest_folder_css + '/*.css'], { base: "." })
+		.pipe(clean_css({ compatibility: 'ie8' }))
+		.pipe(rename({ suffix: ".min." + version }))
+		.pipe(gulp.dest(dest_folder_build));
+
+	gulp.src([dest_folder_js + '/*.js'], { base: "." })
+		.pipe(include({ extensions: "js" }))
+		.pipe(rename({ suffix: ".min." + version }))
+		.pipe(uglify())
+		.pipe(gulp.dest(dest_folder_build));
 
 	gulp.src(['*.html'])
-		.pipe(include({ extensions: "html" }))
+		.pipe(htmlreplace({
+			'css': "./css/style.min." + version + '.css',
+			'jsApp': './js/app.min.' + version + '.js',
+			'jsSite': './js/site-scripts.min.' + version + '.js',
+		}))
+		.pipe(htmlmin({ collapseWhitespace: true }))
 		.pipe(gulp.dest(dest_folder_build))
+
+
 });
 
 
