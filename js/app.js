2cc6adca066ac34689e219a73c3e3f355d714d00
Darius Devenas <d.devenas@adform.com>
2017-08-11 15:28:39 +0300

    adding second package of insurance

--- js/app.js
+++ js/app.js
@@ -1,10 +1,13 @@
 
 const Web3 = require('web3');
-const contractAddress = '0x554ab5f5fe7006223c2dd321b420e9a05d48ec93';
+const contractAddress_v1 = '0x554ab5f5fe7006223c2dd321b420e9a05d48ec93';
+const contractAddress_v2 = '0x554ab5f5fe7006223c2dd321b420e9a05d48ec94';
+const etherscanAddress = 'https://ropsten.etherscan.io/';
 
 var accounts;
 var account;
-var policyContract;
+var policyContract_v1;
+var policyContract_v2;
 
 function chunkWalletAddress(str) {
   var ret = [];
@@ -27,12 +30,20 @@ window.App = {
   start: function () {
     var self = this;
 
-    var abi = JSON.parse('[ { "constant": false, "inputs": [ { "name": "policyOwner", "type": "address" } ], "name": "confirmPolicy", "outputs": [], "payable": false, "type": "function" }, { "constant": true, "inputs": [], "name": "totalInsurers", "outputs": [ { "name": "", "type": "uint256" } ], "payable": false, "type": "function" }, { "constant": false, "inputs": [ { "name": "itemId", "type": "string" }, { "name": "deviceBrand", "type": "string" }, { "name": "deviceYear", "type": "string" }, { "name": "wearLevel", "type": "string" }, { "name": "region", "type": "string" } ], "name": "insure", "outputs": [ { "name": "insured", "type": "bool" } ], "payable": true, "type": "function" }, { "constant": false, "inputs": [ { "name": "wearLevel", "type": "uint256" } ], "name": "claim", "outputs": [ { "name": "", "type": "bool" } ], "payable": false, "type": "function" }, { "constant": true, "inputs": [], "name": "getPolicyEndDateTimestamp", "outputs": [ { "name": "", "type": "uint256" } ], "payable": false, "type": "function" }, { "constant": true, "inputs": [], "name": "totalInvestedAmount", "outputs": [ { "name": "", "type": "uint256" } ], "payable": false, "type": "function" }, { "constant": true, "inputs": [], "name": "totalClaimsPaid", "outputs": [ { "name": "", "type": "uint256" } ], "payable": false, "type": "function" }, { "constant": true, "inputs": [ { "name": "", "type": "address" } ], "name": "investors", "outputs": [ { "name": "", "type": "uint256" } ], "payable": false, "type": "function" }, { "constant": true, "inputs": [], "name": "totalInvestorsCount", "outputs": [ { "name": "", "type": "uint256" } ], "payable": false, "type": "function" }, { "constant": true, "inputs": [], "name": "getPolicyNextPayment", "outputs": [ { "name": "", "type": "uint256" } ], "payable": false, "type": "function" }, { "constant": true, "inputs": [], "name": "basePremium", "outputs": [ { "name": "", "type": "uint256" } ], "payable": false, "type": "function" }, { "constant": true, "inputs": [ { "name": "deviceBrand", "type": "string" }, { "name": "deviceYear", "type": "string" }, { "name": "wearLevel", "type": "string" }, { "name": "region", "type": "string" } ], "name": "policyPrice", "outputs": [ { "name": "price", "type": "uint256" } ], "payable": false, "type": "function" }, { "constant": true, "inputs": [], "name": "maxPayout", "outputs": [ { "name": "", "type": "uint256" } ], "payable": false, "type": "function" }, { "constant": true, "inputs": [], "name": "claimed", "outputs": [ { "name": "", "type": "bool" } ], "payable": false, "type": "function" }, { "constant": false, "inputs": [], "name": "invest", "outputs": [ { "name": "success", "type": "bool" } ], "payable": true, "type": "function" }, { "inputs": [], "payable": true, "type": "constructor" }, { "payable": true, "type": "fallback" }, { "anonymous": false, "inputs": [ { "indexed": false, "name": "deviceName", "type": "string" }, { "indexed": false, "name": "insurancePrice", "type": "uint256" } ], "name": "Insured", "type": "event" }, { "anonymous": false, "inputs": [ { "indexed": false, "name": "payout", "type": "uint256" } ], "name": "Claimed", "type": "event" }, { "anonymous": false, "inputs": [ { "indexed": false, "name": "value", "type": "uint256" } ], "name": "Invested", "type": "event" } ]');
-    var contract = web3.eth.contract(abi);
+    var abi_v1 = JSON.parse('[ { "constant": false, "inputs": [ { "name": "policyOwner", "type": "address" } ], "name": "confirmPolicy", "outputs": [], "payable": false, "type": "function" }, { "constant": true, "inputs": [], "name": "totalInsurers", "outputs": [ { "name": "", "type": "uint256" } ], "payable": false, "type": "function" }, { "constant": false, "inputs": [ { "name": "itemId", "type": "string" }, { "name": "deviceBrand", "type": "string" }, { "name": "deviceYear", "type": "string" }, { "name": "wearLevel", "type": "string" }, { "name": "region", "type": "string" } ], "name": "insure", "outputs": [ { "name": "insured", "type": "bool" } ], "payable": true, "type": "function" }, { "constant": false, "inputs": [ { "name": "wearLevel", "type": "uint256" } ], "name": "claim", "outputs": [ { "name": "", "type": "bool" } ], "payable": false, "type": "function" }, { "constant": true, "inputs": [], "name": "getPolicyEndDateTimestamp", "outputs": [ { "name": "", "type": "uint256" } ], "payable": false, "type": "function" }, { "constant": true, "inputs": [], "name": "totalInvestedAmount", "outputs": [ { "name": "", "type": "uint256" } ], "payable": false, "type": "function" }, { "constant": true, "inputs": [], "name": "totalClaimsPaid", "outputs": [ { "name": "", "type": "uint256" } ], "payable": false, "type": "function" }, { "constant": true, "inputs": [ { "name": "", "type": "address" } ], "name": "investors", "outputs": [ { "name": "", "type": "uint256" } ], "payable": false, "type": "function" }, { "constant": true, "inputs": [], "name": "totalInvestorsCount", "outputs": [ { "name": "", "type": "uint256" } ], "payable": false, "type": "function" }, { "constant": true, "inputs": [], "name": "getPolicyNextPayment", "outputs": [ { "name": "", "type": "uint256" } ], "payable": false, "type": "function" }, { "constant": true, "inputs": [], "name": "basePremium", "outputs": [ { "name": "", "type": "uint256" } ], "payable": false, "type": "function" }, { "constant": true, "inputs": [ { "name": "deviceBrand", "type": "string" }, { "name": "deviceYear", "type": "string" }, { "name": "wearLevel", "type": "string" }, { "name": "region", "type": "string" } ], "name": "policyPrice", "outputs": [ { "name": "price", "type": "uint256" } ], "payable": false, "type": "function" }, { "constant": true, "inputs": [], "name": "maxPayout", "outputs": [ { "name": "", "type": "uint256" } ], "payable": false, "type": "function" }, { "constant": true, "inputs": [], "name": "claimed", "outputs": [ { "name": "", "type": "bool" } ], "payable": false, "type": "function" }, { "constant": false, "inputs": [], "name": "invest", "outputs": [ { "name": "success", "type": "bool" } ], "payable": true, "type": "function" }, { "inputs": [], "payable": true, "type": "constructor" }, { "payable": true, "type": "fallback" }, { "anonymous": false, "inputs": [ { "indexed": false, "name": "deviceName", "type": "string" }, { "indexed": false, "name": "insurancePrice", "type": "uint256" } ], "name": "Insured", "type": "event" }, { "anonymous": false, "inputs": [ { "indexed": false, "name": "payout", "type": "uint256" } ], "name": "Claimed", "type": "event" }, { "anonymous": false, "inputs": [ { "indexed": false, "name": "value", "type": "uint256" } ], "name": "Invested", "type": "event" } ]');
+    var contract_v1 = web3.eth.contract(abi_v1);
+    policyContract_v1 = contract_v1.at(contractAddress_v1);
 
-    policyContract = contract.at(contractAddress);
+    var abi_v2 = JSON.parse('[ { "constant": false, "inputs": [ { "name": "policyOwner", "type": "address" } ], "name": "confirmPolicy", "outputs": [], "payable": false, "type": "function" }, { "constant": true, "inputs": [], "name": "totalInsurers", "outputs": [ { "name": "", "type": "uint256" } ], "payable": false, "type": "function" }, { "constant": false, "inputs": [ { "name": "itemId", "type": "string" }, { "name": "deviceBrand", "type": "string" }, { "name": "deviceYear", "type": "string" }, { "name": "wearLevel", "type": "string" }, { "name": "region", "type": "string" } ], "name": "insure", "outputs": [ { "name": "insured", "type": "bool" } ], "payable": true, "type": "function" }, { "constant": false, "inputs": [ { "name": "wearLevel", "type": "uint256" } ], "name": "claim", "outputs": [ { "name": "", "type": "bool" } ], "payable": false, "type": "function" }, { "constant": true, "inputs": [], "name": "getPolicyEndDateTimestamp", "outputs": [ { "name": "", "type": "uint256" } ], "payable": false, "type": "function" }, { "constant": true, "inputs": [], "name": "totalInvestedAmount", "outputs": [ { "name": "", "type": "uint256" } ], "payable": false, "type": "function" }, { "constant": true, "inputs": [], "name": "totalClaimsPaid", "outputs": [ { "name": "", "type": "uint256" } ], "payable": false, "type": "function" }, { "constant": true, "inputs": [ { "name": "", "type": "address" } ], "name": "investors", "outputs": [ { "name": "", "type": "uint256" } ], "payable": false, "type": "function" }, { "constant": true, "inputs": [], "name": "totalInvestorsCount", "outputs": [ { "name": "", "type": "uint256" } ], "payable": false, "type": "function" }, { "constant": true, "inputs": [], "name": "getPolicyNextPayment", "outputs": [ { "name": "", "type": "uint256" } ], "payable": false, "type": "function" }, { "constant": true, "inputs": [], "name": "basePremium", "outputs": [ { "name": "", "type": "uint256" } ], "payable": false, "type": "function" }, { "constant": true, "inputs": [ { "name": "deviceBrand", "type": "string" }, { "name": "deviceYear", "type": "string" }, { "name": "wearLevel", "type": "string" }, { "name": "region", "type": "string" } ], "name": "policyPrice", "outputs": [ { "name": "price", "type": "uint256" } ], "payable": false, "type": "function" }, { "constant": true, "inputs": [], "name": "maxPayout", "outputs": [ { "name": "", "type": "uint256" } ], "payable": false, "type": "function" }, { "constant": true, "inputs": [], "name": "claimed", "outputs": [ { "name": "", "type": "bool" } ], "payable": false, "type": "function" }, { "constant": false, "inputs": [], "name": "invest", "outputs": [ { "name": "success", "type": "bool" } ], "payable": true, "type": "function" }, { "inputs": [], "payable": true, "type": "constructor" }, { "payable": true, "type": "fallback" }, { "anonymous": false, "inputs": [ { "indexed": false, "name": "deviceName", "type": "string" }, { "indexed": false, "name": "insurancePrice", "type": "uint256" } ], "name": "Insured", "type": "event" }, { "anonymous": false, "inputs": [ { "indexed": false, "name": "payout", "type": "uint256" } ], "name": "Claimed", "type": "event" }, { "anonymous": false, "inputs": [ { "indexed": false, "name": "value", "type": "uint256" } ], "name": "Invested", "type": "event" } ]');
+    var contract_v2 = web3.eth.contract(abi_v2);
+    policyContract_v2 = contract_v2.at(contractAddress_v2);
 
-    // Get the initial account balance so it can be displayed.
+    App.fillInvestorPart();
+
+
+  },
+
+  fillInvestorPart: function () {
     web3.eth.getAccounts(function (err, accs) {
       if (err != null) {
         console.log(err);
@@ -63,49 +74,65 @@ window.App = {
         }
       });
 
-      policyContract.investors(account, function (err, value) {
+      App.fillInvestorInvestment();     
+    });
+
+
+    App.fillInvestmentsHeader();
+    App.fillTableLine1();
+
+
+
+  },
+
+  fillInvestorInvestment: function () {
+
+    policyContract_v1.investors(account, function (err, value) {
         if (err != null) {
           console.log(err);
-          alert("There was an error fetching contract.6");
+          alert("There was an error fetching contract.");
           return;
         }
 
         var ethValue = web3.fromWei(value, 'ether');
+        
+        //TODO: add sum up with 2 contract
         $("#invested").html(ethValue.toString());
+
 
         if (ethValue == 0) {
           $("#projectedProfit").html("20% of an investment");
         } else {
           $("#projectedProfit").html((ethValue * 1.2).toFixed(4).toString());
         }
-      });
     });
 
+  },
 
-    policyContract.totalInsurers.call(function (err, value) {
+  fillInvestmentsHeader: function () {
+
+    policyContract_v1.totalInsurers.call(function (err, value) {
       if (err != null) {
         alert("There was an error fetching contract. 5");
         return;
       }
 
-      $("#totalInsurers").html(value.toString());
-      $("#totalInsurers_table").html(value.toString());
+      $("#totalInsurers").html(value.toString());    
     });
 
-    policyContract.totalClaimsPaid.call(function (err, value) {
+    policyContract_v1.totalClaimsPaid.call(function (err, value) {
       if (err != null) {
         console.log(err);
         alert("There was an error fetching contract. 4");
         return;
       }
       var valueEth = web3.fromWei(value.toString(), 'ether');
-      $("#totalClaimsPaid").html(valueEth.toString());
-      $("#totalClaimsPaid_table").html(valueEth.toString());
+      $("#totalClaimsPaid").html(valueEth.toString()); 
     });
 
 
 
-    policyContract.totalInvestedAmount.call(account, function (err, value) {
+    policyContract_v1.totalInvestedAmount.call(account, function (err, value) {
       if (err != null) {
         console.log(err);
         alert("There was an error fetching contract. 3");
@@ -114,24 +141,19 @@ window.App = {
 
       var ethValue = web3.fromWei(value, 'ether');
       $("#totalInvestedAmount").html(ethValue.toString());
-      $("#totalInvestedAmount_table").html(ethValue.toString());
-
-      //$("#predictedProfit_table").html((ethValue * 1.2).toFixed(2).toString());
-      $("#predictedProfit_table").html("20%");
     });
 
-    policyContract.totalInvestorsCount.call(account, function (err, value) {
+    policyContract_v1.totalInvestorsCount.call(account, function (err, value) {
       if (err != null) {
         console.log(err);
         alert("There was an error fetching contract. 2");
         return;
       }
 
-      $("#totalInvestorsCount").html(value.toString());
-      $("#totalInvestorsCount_table").html(value.toString());
+      $("#totalInvestorsCount").html(value.toString());    
     });
 
-    web3.eth.getBalance(contractAddress, function (err, value) {
+    web3.eth.getBalance(contractAddress_v1, function (err, value) {
       if (err != null) {
         console.log(err);
         alert("There was an error fetching contract. 1");
@@ -142,9 +164,62 @@ window.App = {
       $("#totalContractBalance").html((valueEth * 1.0).toFixed(4).toString());
     });
 
-
   },
 
+
+
+
+  fillTableLine1: function () {
+
+
+
+
+     policyContract_v1.totalInsurers.call(function (err, value) {
+      if (err != null) {
+        alert("There was an error fetching contract. 5");
+        return;
+      }
+
+      $("#totalInsurers_table").html(value.toString());
+    });
+
+    policyContract_v1.totalClaimsPaid.call(function (err, value) {
+      if (err != null) {
+        console.log(err);
+        alert("There was an error fetching contract. 4");
+        return;
+      }
+      var valueEth = web3.fromWei(value.toString(), 'ether');
+
+      $("#totalClaimsPaid_table").html(valueEth.toString());
+    });
+
+
+
+    policyContract_v1.totalInvestedAmount.call(account, function (err, value) {
+      if (err != null) {
+        console.log(err);
+        alert("There was an error fetching contract. 3");
+        return;
+      }
+
+      var ethValue = web3.fromWei(value, 'ether');
+  
+      $("#totalInvestedAmount_table").html(ethValue.toString());
+      $("#predictedProfit_table").html("20%");
+    });
+
+    policyContract_v1.totalInvestorsCount.call(account, function (err, value) {
+      if (err != null) {
+        console.log(err);
+        alert("There was an error fetching contract. 2");
+        return;
+      }
+
+      $("#totalInvestorsCount_table").html(value.toString());
+    });
+
+  },
 
   investMoney: function () {
     var self = this;
@@ -152,7 +227,7 @@ window.App = {
     var weiValue = web3.toWei(investValue, 'ether');
     var weiNumber = web3.toBigNumber(weiValue);
 
-    policyContract.invest(weiValue, { gas: 300000, from: account, value: weiNumber }, function (err, result) {
+    policyContract_v1.invest(weiValue, { gas: 300000, from: account, value: weiNumber }, function (err, result) {
 
       if (err != null) {
         console.log(err);
@@ -163,7 +238,7 @@ window.App = {
         return;
       }
       else {
-        var etherscan = "https://ropsten.etherscan.io/tx/" + result;
+        var etherscan = etherscanAddress + "tx/" + result;
         $("#modalTitle").html("Investment Successful");
         $("#modalText").html("Great success! Transaction has been sent. Ethers should apear in the wallet any moment (it might take up to a few minutes). You can check progress here: " + "<br /><br />" + "<a class=\"btn-link\" href=\"" + etherscan + "\" target=\"_blank\">" + etherscan + "</a><br /><br />Ethers should appear in the wallet at any moment.");
         $("#generalModal").modal('show');
@@ -189,7 +264,6 @@ window.addEventListener('load', function () {
     web3.version.getNetwork(function (err, netId) {
       switch (netId) {
         case "3":
-          //console.log('This is the ropsten test network.')
           break
         default:
           App.displayNoWalletModal();
